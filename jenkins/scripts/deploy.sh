f9bacf2a6900309ff360bc232f5c5cb86eea6611
spun <reinaldobust97@gmail.com>
2021-11-10 14:37:07 -0300

    .

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -7,9 +7,9 @@ set -x
 cp build/** /home/vps218/www/
 ls /home/vps218/www
 echo 'Kill container'
-docker kill example-pipeline
+docker kill fedent-implementacion
 echo 'Serve build'
-docker run -dit --name example-pipeline -p 8081:80 -v /home/vps218/www/:/usr/local/apache2/htdocs/ httpd:2.4
+docker run -dit --name fedent-implementacion -p 8081:80 -v /home/vps218/www/:/usr/local/apache2/htdocs/ httpd:2.4
 set +x
 
 echo 'Running container'
